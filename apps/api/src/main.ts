13b2a28a2862cc11ec61fb0bd99f16f923132f64
KostyalBalint <kostyal.balintnak@gmail.com>
2022-09-28 14:37:42 +0200

    CD Demo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { Message } from '@bosch/api-interfaces';
 
 const app = express();
 
-const greeting: Message = { message: 'Welcome to api!' };
+const greeting: Message = { message: 'CD Demo' };
 
 app.get('/api', (req, res) => {
   res.send(greeting);
