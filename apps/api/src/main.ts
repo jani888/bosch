791bcaa28d7510f259f5be1240eb3fe8fb280749
KostyalBalint <kostyal.balintnak@gmail.com>
2022-09-28 14:51:09 +0200

    Change back to OG msg

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import { Message } from '@bosch/api-interfaces';
 
 const app = express();
 
-const greeting: Message = { message: 'CD Demo' };
+const greeting: Message = { message: 'Welcome to api!' };
 
 app.get('/api', (req, res) => {
   res.send(greeting);
